fda744f3c8af898512c00d5d0056bef016f4128f
Aime Didier <aimedidiermugisha@gmail.com>
2024-07-31 20:13:53 +0200

    Authorization

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\ExporterMiddleware;
+use App\Http\Middleware\MinicomMiddleware;
+use App\Http\Middleware\SellerMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'landing');
@@ -9,4 +12,22 @@ ROute::view('/login', 'auth.login')->name('login');
 Route::group(["prefix" => "auth", "as" => "auth."], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
+});
+
+Route::group(["prefix" => "seller", "as" => "seller.", 'middleware' => SellerMiddleware::class], function () {
+    Route::get('/', function () {
+        return "Welcome Seller!";
+    });
+});
+
+Route::group(["prefix" => "exporter", "as" => "exporter.", 'middleware' => ExporterMiddleware::class], function () {
+    Route::get('/', function () {
+        return "Welcome Exporter!";
+    });
+});
+
+Route::group(["prefix" => "minicom", "as" => "minicom.", 'middleware' => MinicomMiddleware::class], function () {
+    Route::get('/', function () {
+        return "Welcome Minicom!";
+    });
 });
